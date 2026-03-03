9daec90a407a0ea34a6dfd76544e98a8ae2719de
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-04 07:02:02 +0900

    *を使ってエクスポートした

--- types/domain/index.ts
+++ types/domain/index.ts
@@ -1,34 +1,7 @@
-export type { ImageId, ItemId, PantryItemId, UserId } from "./common";
-export type {
-  AccountType,
-  Allergen,
-  Buyer,
-  BuyerName,
-  BuyerSetting,
-} from "./user";
-export type {
-  Store,
-  StoreAddress,
-  StoreIconUrl,
-  StoreIntroduction,
-  StoreName,
-  StoreProfile,
-  StoreSetting,
-} from "./user";
-export type { Item, ItemCategory, SortKey } from "./item";
-export type {
-  ItemDetailForBuyer,
-  ItemDetailForStore,
-  ItemViewForBuyer,
-  ItemViewForStore,
-} from "./item";
-export type { Pantry, PantryItem } from "./pantry";
-export type {
-  Chat,
-  ChatMessage,
-  Recipe,
-  Recipes,
-  Role,
-  Material,
-} from "./chat";
-export type { Reports } from "./report";
+export type * from "./common";
+export type * from "./user";
+
+export type * from "./item";
+export type * from "./pantry";
+export type * from "./chat";
+export type * from "./report";
