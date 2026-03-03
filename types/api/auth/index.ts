ff2874b5f0ec3ff2be240eb70d33b57ce904b673
hinattyamann <hinattyamann517@gmail.com>
2026-03-04 04:57:32 +0900

    style: 💄 Formatting

--- types/api/auth/index.ts
+++ types/api/auth/index.ts
@@ -1,4 +1,10 @@
-export type { AuthRegisterPostRequest, AuthRegisterPostResponse } from "./register";
+export type {
+  AuthRegisterPostRequest,
+  AuthRegisterPostResponse,
+} from "./register";
 export type { AuthLoginPostRequest, AuthLoginPostResponse } from "./login";
 export type { AuthSessionGetResponse } from "./session";
-export type { AuthRefreshPostRequest, AuthRefreshPostResponse } from "./refresh";
+export type {
+  AuthRefreshPostRequest,
+  AuthRefreshPostResponse,
+} from "./refresh";
