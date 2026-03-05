56e2cc27ef637bcfb5806ff02447e65598f45d44
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-05 11:10:08 +0900

    ユーザー名がnullの場合の処理の追加

--- components/layout/header/AccountField.tsx
+++ components/layout/header/AccountField.tsx
@@ -7,7 +7,9 @@ interface AccountFieldProps {
 export function AccountField({ userName }: AccountFieldProps) {
   return (
     <div className="flex items-center space-x-2">
-      <span className="text-sm font-medium text-gray-700">{userName} さん</span>
+      <span className="text-sm font-medium text-gray-700">
+        {userName ?? "名無し"} さん
+      </span>
     </div>
   );
 }
