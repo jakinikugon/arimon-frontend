f101573bbd2289e53ead4562a5701a09d0dd457f
hinattyamann <hinattyamann517@gmail.com>
2026-03-05 16:15:07 +0900

    feat: 🎸 AbortSignalを追加し、suggestionsにおいてキャンセル可能な連続リクエスト制御を行った。

--- services/pantry/suggestions.ts
+++ services/pantry/suggestions.ts
@@ -5,10 +5,14 @@ import { queryString } from "@/utils/url";
 import { backendApiUrl } from "@/lib/api";
 import { fetcher } from "@/lib/fetcher";
 
-export async function getPantrySuggestionsQuery(q: string) {
+export async function getPantrySuggestionsQuery(
+  q: string,
+  signal?: AbortSignal,
+) {
   const path = queryString("/api/pantry/suggestions", { q });
 
   return fetcher<PantrySuggestionsGetResponse>(backendApiUrl(path), {
     method: "GET",
+    signal,
   });
 }
