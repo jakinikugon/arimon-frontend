501d813dbe1d9320369ab60436438c73c383687b
yum <yoshiayu4@icloud.com>
2026-03-06 00:23:42 +0900

    登録ページ作成&path更新

--- components/layout/header/LoginButton.tsx
+++ components/layout/header/LoginButton.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export function LoginButton() {
   return (
-    <Link href="/login">
-      <button className="bg-brand-main hover:bg-brand-600 focus:ring-brand-light rounded-md px-4 py-2 text-sm font-medium text-white focus:ring-2 focus:ring-offset-2 focus:outline-none">
+    <Link href="/auth/login">
+      <button className="bg-brand-main-400 hover:bg-brand-600 focus:ring-brand-light rounded-md px-4 py-2 text-sm font-medium text-white focus:ring-2 focus:ring-offset-2 focus:outline-none">
         ログイン
       </button>
     </Link>
