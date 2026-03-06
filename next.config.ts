e84f1c03e7289f019796522b5b3ba229f2e66714
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-06 12:00:37 +0900

    バックエンドのドメインを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+import { EnvConfig } from "./lib/env";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [new URL(EnvConfig.api.backend).hostname],
+  },
 };
 
 export default nextConfig;
