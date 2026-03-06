9a3a00a3bf0f7723307179c5b981628e9cfeb304
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-06 12:05:44 +0900

    直書きで泣きの顔つき

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-import { EnvConfig } from "./lib/env";
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [new URL(EnvConfig.api.backend).hostname],
+    domains: ["arimon-backend.rayfiyo.com"],
   },
 };
 
